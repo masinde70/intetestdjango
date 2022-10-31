2edd696f6d97c30546e5fb77fd49ba54afb54be3
masinde70 <5812481+masinde70@users.noreply.github.com>
2022-10-31 09:48:39 +0200

    test user creation

--- integrify/accounts/tests.py
+++ integrify/accounts/tests.py
@@ -2,3 +2,18 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 # Create your tests here.
+
+class CustomTest(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create(
+            username = "masinde", email = "masinde@your-email.com",password = 
+            "test123",
+        )
+        self.assertEqual(user.username, "masinde")
+        self.assertEqual(user.email, " masinde@your-email.com")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    
